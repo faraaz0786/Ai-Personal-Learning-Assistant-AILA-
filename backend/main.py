181f3fb85eb6512623899a76ffa99e2a31ada9ee
Faraaz <faraazkalim9@gmail.com>
2026-03-28 08:28:50 +0530

    feat: enable CORS

--- backend/main.py
+++ backend/main.py
@@ -4,3 +4,14 @@ from api.v1.router import api_router
 app = FastAPI()
 
 app.include_router(api_router)
+
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # later restrict to frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
